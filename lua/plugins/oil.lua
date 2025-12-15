ef0b1179780cb4d76465cd21a011a5a8c0886748
kelvin <kelvin@kelvins-MacBook-Air.local>
2025-12-15 02:04:00 -0500

    changed oil keymappings to not interfere with harpoon

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,9 +1,13 @@
 return	{
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-  --dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer
-  lazy = false,
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = { keymaps = {
+        ['<C-h>'] = false, -- Disables horizontal split
+        ['<C-t>'] = false, -- Disables opening in a new tab
+        ['<C-s>'] = false, -- Disables vertical split
+    }},
+    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+    --dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer
+    lazy = false,
 }
