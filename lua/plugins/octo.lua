59b2c289f7f59f9e6f9f5daa3af014114b3dbf91
KelvinCODES <kelvinkong.cg@gmail.com>
2025-12-17 00:19:14 -0500

    added comments for default_merge_method and default_delete_branch just (#2)
    incase
    
    Co-authored-by: kelvin <kelvin@kelvins-MacBook-Air.local>

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -9,12 +9,14 @@ return {
 	opts = {
 		use_local_fs = true, -- Use local files on the right side of reviews
 		picker = "snacks", -- Explicitly set snacks as the picker
+		-- default_merge_method = "squash", -- options: "merge", "rebase", "squash"
+		-- default_delete_branch = true, -- automatically deletes the branch after merge
 	},
 	keys = {
 		{ "<leader>op", "<cmd>Octo pr list<cr>", desc = "Octo PR List" },
-        { "<leader>oP", "<cmd>Octo pr create<cr>", desc = "Create Pull Request" }, -- Added this
+		{ "<leader>oP", "<cmd>Octo pr create<cr>", desc = "Create Pull Request" }, -- Added this
 
-        --- remove later if still unused
+		--- remove later if still unused
 		---{ "<leader>oi", "<cmd>Octo issue list<cr>", desc = "Octo Issue List" },
 		---{ "<leader>os", "<cmd>Octo search<cr>", desc = "Octo Search" },
 		-- Buffer-local maps for PRs (only active in octo filetypes)
